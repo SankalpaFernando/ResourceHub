bf8b6bdf49d7592f0052cbf662dae59005b53efb
Sankalpa Fernando <55610818+SankalpaFernando@users.noreply.github.com>
2023-08-29 09:19:16 +0530

    Password Credential Removed

--- email/confirm.php
+++ email/confirm.php
@@ -21,7 +21,7 @@ function createMailInstance(){
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'resourcehub.connect@gmail.com';                     //SMTP username
-        $mail->Password   = 'bgnjgzlukcgbucfg';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 

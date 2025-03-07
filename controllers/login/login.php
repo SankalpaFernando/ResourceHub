2b4f4df7f66b7a7f5b5c8c9a161b13aef319fb7e
Sankalpa Fernando <SankalpaFernando2017@gmail.com>
2025-03-07 21:07:57 +0530

    Login Issue Fixed

--- controllers/login/login.php
+++ controllers/login/login.php
@@ -1,13 +1,14 @@
-
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once "../../database/connection.php";
 require_once "../util/util.php";
-
 
 if(isset($_GET["login"])){
 
     $data = json_decode(file_get_contents("php://input"),true);
-
 
     $email = $data["email"];
     $password = $data["password"];
@@ -20,85 +21,49 @@ if(isset($_GET["login"])){
         $row = $result->fetch_assoc();
         if(!password_verify($password,$row["USER_PASSWORD"])){
             echo "wrong";
-             return;
-         }
+            return;
+        }
         $query ="SELECT * FROM user WHERE USER_EMAIL='$email' AND APPROVED=1 ";
-
 
         $res = $conn->query($query);
 
         if($res && $res->num_rows > 0){
-            
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
             $_SESSION["user_id"] = $row["USER_ID"];
             $_SESSION["user_name"] = $row["USER_NAME"];
             $_SESSION["user_email"] = $row["USER_EMAIL"];
             $_SESSION["user_type"] = $row["USER_TYPE"];
 
             echo "success";
-
             return;
         }
         echo "notapproved";
-
-       
-        
-    }else{
-
+    } else {
         echo "wrong";
-
     }
 
     return;
 }
 
-
 if(isset($_GET['register'])){
 
-
-    $data=json_decode(file_get_contents("php://input"),true);
+    $data = json_decode(file_get_contents("php://input"),true);
 
     $username = $data["username"];
     $email = $data["email"];
     $account_type = $data["account_type"];
     $password = $data["password"];
     $studentID = $data["student_id"];
-    $password = password_hash($password,PASSWORD_DEFAULT);
-
+    $password = password_hash($password, PASSWORD_DEFAULT);
 
     $invalid = false;
-    
-    print_r($username);
 
-    // if(isEmailExists($conn,$email)==true){
-    //     $invalid = true;
-    // }
-    // if($account_type == "STUDENT"){
-    //     $studentID = $data["student_id"];
-    //     if(isStudentIDExists($conn,$studentID)){
-    //         $invalid = true;
-    //     }
-    // }else{
-    //     $studentID = "";
-    // }
-
-    // print_r("is Inavalid:" . $invalid);
-    // if($invalid){
-    //     echo "false";
-    //     return;
-    // }
-  
     $query = "INSERT INTO user (USER_NAME, USER_EMAIL, STUDENT_ID, USER_PASSWORD, USER_TYPE) VALUES ('$username', '$email', '$studentID', '$password', '$account_type')";
-    print_r($query);
+    
     $result = $conn->query($query);
-
-    print_r($result);
     
     if($result){
         echo "success";
-    }else{
+    } else {
         echo "false";
     }
 
@@ -106,15 +71,10 @@ if(isset($_GET['register'])){
 }
 
 if(isset($_GET["logout"])){
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
     session_destroy();
     $_SESSION = array();
     header("Location: ../../views/login/view.php");
+    exit(); // Make sure to stop further execution after header redirection.
     return;
 }
-
-
-
 ?>
